79f0f9b0af90a10c81b978d349d5547d9c85bfba
Penny Kanyua-Muguku <ctrl-jaz>
2024-11-14 11:23:28 +0300

    fixed imports

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//use App\http\Controllers\Authcontroller;
+use App\http\Controllers\Authcontroller;
 use App\http\Controllers\DonationsController;
 use App\http\Controllers\CategoriesController;
 use App\http\Controllers\RequestsController;
 
+Route::post ('/register', [Authcontroller::class, 'register']);
+Route::post ('/login', [Authcontroller::class, 'login']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::apiResources([
-    'categories'=>CategoriesController::class,
-    'donations'=>DonationsController::class,
-    'requests'=>RequestsController::class,
 
-]);
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::apiResources([
+        'categories'=>CategoriesController::class,
+        'donations'=>DonationsController::class,
+        'requests'=>RequestsController::class,
+
+    ]);
+
+
+    Route::get('/getDonations/{id}',[Donationscontroller::class, 'getDonations']);
+});
